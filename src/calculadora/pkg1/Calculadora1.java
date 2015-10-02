60b09cc21fd43f829eb4ef5b28a8163c45cb3c4a
felipejara <f.jara03@ufromail.cl>
2015-10-02 09:47:05 -0300

    calculadora1

--- src/calculadora/pkg1/Calculadora1.java
+++ src/calculadora/pkg1/Calculadora1.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package calculadora.pkg1;
-
+import java.util.*;
 /**
  *
  * @author Felipe Jara
@@ -16,6 +16,105 @@ public class Calculadora1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        int operacion, operacionb;//operacionb = operacion basica
+        double numero1, numero2, resultado;
+        Scanner leer = new Scanner(System.in);
+        do{
+        System.out.println("ingrese la operacion que desea realizar, 1=operaciones basicas, 2=mayor de dos numeros, 3=menor de dos numeros, 4=potencia de un numero, 5=porcentaje de un numero.");
+         operacion = leer.nextInt();
+        }while(operacion<1 || operacion>5);
+         if(operacion == 1){
+        
+             System.out.println("ingrese: 1=suma, 2=resta, 3=multiplicacion, 4=division");
+             operacionb = leer.nextInt();
+             
+           if(operacionb == 1){  
+            System.out.println("ingrese el primer numero");
+             numero1 = leer.nextDouble();
+             
+             System.out.println("ingrese el segundo numero");
+             numero2 = leer.nextDouble();
+             
+             resultado = numero1 + numero2;
+             
+             System.out.println("el resultado es: "+ resultado);
+
+         }else{
+               if(operacionb == 2){
+                   System.out.println("ingrese el primer numero");
+             numero1 = leer.nextDouble();
+             
+             System.out.println("ingrese el segundo numero");
+             numero2 = leer.nextDouble();
+             
+             resultado = numero1 - numero2;
+             
+             System.out.println("el resultado es: "+ resultado);
+
+               }else{
+                   if(operacionb == 3){
+                       System.out.println("ingrese el primer numero");
+             numero1 = leer.nextDouble();
+             
+             System.out.println("ingrese el segundo numero");
+             numero2 = leer.nextDouble();
+             
+             resultado = numero1 * numero2;
+             
+             System.out.println("el resultado es: "+ resultado);
+
+                   }else{
+                       if(operacionb == 4){
+                           
+                           System.out.println("ingrese el primer numero");
+                             numero1 = leer.nextDouble();
+             
+                             System.out.println("ingrese el segundo numero");
+                              numero2 = leer.nextDouble();
+             
+                               resultado = numero1 / numero2;
+              
+                               System.out.println("el resultado es: "+ resultado);
+
+                           
+                       }
+                   }
+               }
+           }
+           
+         
+         
+         
+    }else{
+             if(operacion == 2){
+                 System.out.println("ingrese el primer numero");
+                  numero1 = leer.nextDouble();
+             
+                 System.out.println("ingrese el segundo numero");
+                  numero2 = leer.nextDouble();
+                  if(numero1 > numero2){
+                      System.out.println();
+                  }
+                 
+             }
+         }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     }
     
 }
