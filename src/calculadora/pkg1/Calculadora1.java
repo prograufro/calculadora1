e6867db803fa374d40b08210998fdefb28b80244
felipejara <f.jara03@ufromail.cl>
2015-10-02 16:18:33 -0300

    calculadora1 version validada

--- src/calculadora/pkg1/Calculadora1.java
+++ src/calculadora/pkg1/Calculadora1.java
@@ -17,18 +17,27 @@ public class Calculadora1 {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        int operacion, operacionb;//operacionb = operacion basica
+        int operacion, operacionb, seguir ;//operacionb = operacion basica
         double numero1, numero2, resultado;
-        Scanner leer = new Scanner(System.in);
-        do{
-        System.out.println("ingrese la operacion que desea realizar, 1=operaciones basicas, 2=mayor y menor de dos numeros, 3=potencia de un numero, 4=porcentaje de un numero.");
-         operacion = leer.nextInt();
-        }while(operacion<1 || operacion>5);
-         if(operacion == 1){
         
+        Scanner leer = new Scanner(System.in);
+        
+        try{   
+            
+     do{    
+        
+         do{
+        System.out.println("ingrese la operacion que desea realizar, 1=operaciones basicas, 2=mayor y menor de dos numeros, 3=potencia de un numero, 4=porcentaje de un numero."); 
+        operacion = leer.nextInt();
+        }while(operacion<1 || operacion>5);
+        
+        if(operacion == 1){//si operacion = 1 ingresa a operaciones basicas
+         do{
              System.out.println("ingrese: 1=suma, 2=resta, 3=multiplicacion, 4=division");
              operacionb = leer.nextInt();
-             
+      
+         }while(operacionb<1 || operacionb>4);
+         
            if(operacionb == 1){  
             System.out.println("ingrese el primer numero");
              numero1 = leer.nextDouble();
@@ -38,7 +47,7 @@ public class Calculadora1 {
              
              resultado = numero1 + numero2;
              
-             System.out.println("el resultado es: "+ resultado);
+             System.err.println("el resultado de la suma es: "+ resultado);
 
          }else{
                if(operacionb == 2){
@@ -50,7 +59,7 @@ public class Calculadora1 {
              
              resultado = numero1 - numero2;
              
-             System.out.println("el resultado es: "+ resultado);
+             System.err.println("el resultado de la resta es: "+ resultado);
 
                }else{
                    if(operacionb == 3){
@@ -62,20 +71,25 @@ public class Calculadora1 {
              
              resultado = numero1 * numero2;
              
-             System.out.println("el resultado es: "+ resultado);
+             System.err.println("el resultado de la multiplicación es: "+ resultado);
 
                    }else{
                        if(operacionb == 4){
                            
                            System.out.println("ingrese el primer numero");
                              numero1 = leer.nextDouble();
-             
+             do{
                              System.out.println("ingrese el segundo numero");
                               numero2 = leer.nextDouble();
-             
+                              if(numero2 == 0){
+                                 System.err.println("NO SE PUEDE DIVIDIR POR 0");
+                                 System.out.println();
+                              }
+                              
+             }while(numero2 == 0);
                                resultado = numero1 / numero2;
               
-                               System.out.println("el resultado es: "+ resultado);
+                               System.err.println("el resultado de la división es: "+ resultado);
 
                            
                        }
@@ -94,13 +108,13 @@ public class Calculadora1 {
                  System.out.println("ingrese el segundo numero");
                   numero2 = leer.nextDouble();
                   if(numero1 > numero2){
-                      System.out.println("el numero" +numero1+ "es el mayor");
+                      System.err.println("el numero" +numero1+ "es el mayor");
                        System.out.println();
-                        System.out.println("el numero " +numero2+ "es el menor ");
+                        System.err.println("el numero " +numero2+ "es el menor ");
                   }else{
-                      System.out.println("el numero " +numero2+ " es mayor");
+                      System.err.println("el numero " +numero2+ " es mayor");
                        System.out.println();
-                        System.out.println("el numero " +numero1+ "es el menor");
+                        System.err.println("el numero " +numero1+ "es el menor");
                   }
                  
              }else{
@@ -113,29 +127,43 @@ public class Calculadora1 {
                   
                   resultado = (double) Math.pow(numero1,numero2); 
                   
-                  System.out.println("el resultado es: "+resultado);
+                  System.err.println("el resultado de la potencia es: "+resultado);
                      
                  }else{
                      if(operacion == 4){
                          
-                         System.out.println("ingrese el primer numero");
+                         System.out.println("ingrese el porcentaje");
                              numero1 = leer.nextDouble();
              
-                         System.out.println("ingrese el segundo numero");
+                         System.out.println("ingrese el numero a sacar el porcentaje");
                              numero2 = leer.nextDouble();
                          
+                             resultado = (numero2 * numero1)/100 ;
+                             System.err.println("el "+numero1+"% de "+numero2+" es: "+resultado);
                          
                      }
                  }
              }
          }
+        
+         do{ 
+       System.out.println();
+        System.out.println("¿Desea realizar otra operacion? 1=si, 2=no");
+        
+        seguir = leer.nextInt();
+        
+        System.out.println();
+        
+         }while(seguir<1 || seguir>2);
+         
+    }while(seguir == 1);
     
     
     
-    
-    
-    
-    
+        } catch (InputMismatchException in){
+            System.err.println("ERROR, PORFAVOR INGRESE UN NUMERO");
+            System.out.println();
+        }
     
     
     
